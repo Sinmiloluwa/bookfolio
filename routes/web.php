b41c9d228265df1e1e0363a8b97d477df18c8d61
Sinmiloluwa <mofeoluwae@gmail.com>
2021-09-08 07:44:19 -0700

    Changed route for homepage

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\BookController::class, 'index']);
 Route::get('/books', [App\Http\Controllers\BookController::class, 'discover'])->name('books.discover');
 Route::get('/books/{bookId}', [App\Http\Controllers\BookController::class, 'view'])->name('books.view');
